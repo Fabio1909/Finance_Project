017499cfb628dfd3240da3bb7318820b12eee991
Fabio Stefana <fabio.stefana@syrto.ai>
2024-11-20 13:14:54 +0100

    Plotting quasi fatto, ma senza moving average

--- utils/OHLC.py
+++ utils/OHLC.py
@@ -19,17 +19,26 @@ class OHLC():
         self.current_window = self.price_info.iloc[start:end].copy()
         self.horizon_start = self.price_info.iloc[horizon_start]
         self.horizon_end = self.price_info.iloc[horizon_end]
-        self.m_avg_data = self.price_info.iloc[past_start:end].copy()
+        self.m_avg_data = self.price_info.iloc[past_start:end]["Close"].copy()
         self.volume = self.current_window["Vol"].copy()
         self.current_window.drop(["Vol"], axis = 1, inplace = True)
 
+    def calculate_moving_avg(self): 
+        self.moving_average = [] 
+        for i in range(0, len(self.m_avg_data) - self.window_size):
+            avg_window = self.m_avg_data.iloc[i:i+self.window_size]
+            avg_value = avg_window.mean()
+            self.moving_average.append(avg_value)
+
     def step2_scale_window(self):
-        _max = max(self.current_window.max())
-        _min = min(self.current_window.min())
+        _max = max(max(self.current_window.max()), max(self.moving_average))
+        _min = min(min(self.current_window.min()), min(self.moving_average))
         self.current_window_scaled = self.current_window.to_numpy()
         self.current_window_scaled = np.round(((self.current_window_scaled - _min)/(_max - _min))*99)
         self.current_window_scaled = self.current_window_scaled.astype(int)
         self.current_window_scaled = 99 - self.current_window_scaled
+        # moving avg
+        self.moving_avg_scaled = [int(round(((item - _min)/(_max - _min))*99)) for item in self.moving_average]
 
     def __scale_volume(self):
         _max = max(self.volume)
@@ -39,7 +48,7 @@ class OHLC():
 
     def step3_create_image_object(self):
         self.width, self.height = len(self.current_window_scaled) * 3, 125
-        self.image = Image.new("RGB", (self.width, self.height), "black")
+        self.image = Image.new("L", (self.width, self.height), "black")
         self.pixels = self.image.load()
 
     def step4_draw_price_on_image(self):
@@ -63,18 +72,18 @@ class OHLC():
 
         # Draw Open
         if self.candle_px_counter == 1:
-            self.pixels[x, open_px] = (255, 255, 255)
+            self.pixels[x, open_px] = (255)
             self.candle_px_counter += 1
         # Draw Min Max line
         elif self.candle_px_counter == 2:
             start_body = min(max_px, min_px)
             end_body = max(max_px, min_px)
             for y in range(start_body, end_body + 1):
-                self.pixels[x, y] = (255, 255, 255)
+                self.pixels[x, y] = (255)
             self.candle_px_counter += 1
         # Draw Close
         elif self.candle_px_counter == 3:
-            self.pixels[x, close_px - 1] = (255, 255, 255)
+            self.pixels[x, close_px - 1] = (255)
             self.candle_px_counter = None
 
     def step5_draw_volume(self):
@@ -82,12 +91,14 @@ class OHLC():
         for idx, x in enumerate(range(1, self.width, 3)):
             for y in range(self.height):
                 if y > self.height - self.scaled_vol[idx]:
-                    self.pixels[x, y] = (255, 255, 255)
+                    self.pixels[x, y] = (255)
 
-    def calculate_moving_avg(self):
-        for i in range(0, len(self.m_avg_data) - sel.window_size):
-            avg_window = self.m_avg_data.iloc[] ## DA FINIRE
-    
+    def draw_moving_average(self):
+        for x in range(self.width):
+            idx = x // 3
+            current_ma_level = self.moving_avg_scaled[idx]
+            self.pixels[x, current_ma_level] = (255)
+   
     def step6_annotate_image(self):
         start_price = self.horizon_start["Open"]
         end_price = self.horizon_end["Close"]
@@ -103,10 +114,25 @@ class OHLC():
 
     def plot_window(self, start, end, horizon_start, horizon_end, past_start, directory_not_complete):
         self.step1_choose_window_to_plot(start, end, horizon_start, horizon_end, past_start)
+        # Some checks to ensure everything works as intended
+        if self.m_avg_data.empty:
+            print("Skipping plot as m_avg_data is empty.")
+            return  
+        if self.current_window.isnull().any():
+            print("Skipping plot as current_window contains NaN values.")
+            return
+        if self.horizon_start is None or self.horizon_end is None:
+            print("Either horizon_start or horizon_end is None.")
+            return
+        if self.m_avg_data.isnull().any():
+            print("Skipping plot as moving average data contains NaN values.")
+
+        self.calculate_moving_avg()
         self.step2_scale_window()
         self.step3_create_image_object()
         self.step4_draw_price_on_image()
         self.step5_draw_volume()
+        #self.draw_moving_average()
         self.step6_annotate_image()
         self.step7_save_img(directory_not_complete)
         self.step8_clear_window()
